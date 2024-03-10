63098689dd742ef1e62413a5a46b2503df99e341
RielChan <medeia_beliar@naver.com>
2024-03-10 18:11:14 +0900

    search user archive with v-archive

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ import Archive from "./pages/archive"
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="App">
+			<div className="App" id="app">
 				<Navbar bg="dark" data-bs-theme="dark">
 					<Container>
 						<Navbar.Brand href="/">디제이맥스 위키</Navbar.Brand>
 						<Nav className="me-auto">
-							<Nav.Link href="songs">수록곡</Nav.Link>
+							<Nav.Link href="/songs">수록곡</Nav.Link>
 							<Nav.Link>패턴</Nav.Link>
-							<Nav.Link href="archive">성과표</Nav.Link>
+							<Nav.Link href="/archive">성과표</Nav.Link>
 						</Nav>
 						<Nav className="ms-auto">
 							<NavDropdown title="계정">
-								<NavDropdown.Item href="login">로그인</NavDropdown.Item>
-								<NavDropdown.Item href="register">회원가입</NavDropdown.Item>
+								<NavDropdown.Item href="/login">로그인</NavDropdown.Item>
+								<NavDropdown.Item href="/register">회원가입</NavDropdown.Item>
 							</NavDropdown>
 						</Nav>
 					</Container>
@@ -31,7 +31,8 @@ function App() {
 					<Route path="/songs" element={<Songs />}></Route>
 					<Route path="/login" element={<Login />}></Route>
 					<Route path="/archive" element={<Archive />}></Route>
-					<Route path="/archive/:user/:button/:difficulty" element={<Archive />}></Route>
+					<Route path="/archive/:user/board/" element={<Archive />}></Route>
+					<Route path="/archive/:user/board/:button/:difficulty" element={<Archive />}></Route>
 				</Routes>
 			</div>
 		</BrowserRouter>
--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 import { useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, redirect } from 'react-router-dom';
 
+function Search(event) {
+	event.preventDefault();
+	const data = `${event.target[0].value}/board/${event.target[1].value}/${event.target[2].value}`
+	let search = encodeURI(`archive/${data}`)
+	document.head.innerHTML = `<meta http-equiv="refresh" content="0;URL='${search}'" />`
+}
 
 function Archive() {
 	const [data, setData] = useState(null);
 	const params = useParams()
-	if (params.user) {
+	if (params.button) {
 		axios.get(
 			`https://v-archive.net/api/archive/${params.user}/board/${params.button}/${params.difficulty}`,
 			{
@@ -30,20 +36,21 @@ function Archive() {
 	else {
 		return (
 			<>
-				<form action="/">
-					<input type="text" placeholder="닉네임"></input>
-					<select name="버튼">
+				<form onSubmit={Search}>
+					<input type="text" placeholder="닉네임" name="user"></input>
+					<select name="button">
 						<option value='4B'>4B</option>
 						<option value='5B'>5B</option>
 						<option value='6B'>6B</option>
 						<option value='8B'>8B</option>
 					</select>
-					<select name="난이도" defaultValue='SC'>
+					<select name="difficulty" defaultValue='SC'>
 						<option value='NM'>NM</option>
 						<option value='HD'>HD</option>
 						<option value='MX'>MX</option>
 						<option value='SC'>SC</option>
 					</select>
+					<button type='submit'>검색</button>
 				</form>
 			</>
 		)
